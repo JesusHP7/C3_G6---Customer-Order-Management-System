97a1235d08327f60be2655f79bd740ac354b2982
joan <estremorjoan@gmail.com>
2026-03-20 08:44:36 -0500

    finally

--- Order_Creation.py
+++ Order_Creation.py
@@ -14,3 +14,24 @@ def agg_order(clients, products):
 
   if product_id not in products:
     return "Product not found"
+  
+# ask quantity
+  quantity = int(input("Enter quantity: "))
+
+  product = products[product_id]
+
+  # calculate total price
+  total = product[1] * quantity
+
+  # create order id
+  order_id = len(clients[client_id]["order"]) + 1
+
+  # save order
+  clients[client_id]["order"][order_id] = {
+    "product": product[0],
+    "price": product[1],
+    "quantity": quantity,
+    "total": total
+    }
+
+  return "Order created"
