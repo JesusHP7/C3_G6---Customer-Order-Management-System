bbcaf8c82617b947fec914d69c57bad01cc3da13
Kevin <kevinmercao1912000@icloud.com>
2026-03-20 06:43:56 -0500

    still working ...

--- register_product.py
+++ register_product.py
@@ -1,36 +1,45 @@
-registered_products = {}
+registered_products = {
+    1234: ("manzana", 1000),
+
+}
 
 def unit_price():
-    for i in range(3):      
-        try:
-            price = float(input("Enter Unit Price: "))
-            if price <= 0:
-                print("Unit Price cannot be negative or zero. Please try again.")
-                continue
-            return price
-        except ValueError:
+  for i in range(3):      
+    try:
+      price = float(input("Enter Unit Price: "))
+      if price <= 0:
+        print("Unit Price cannot be negative or zero. Please try again.")
+        continue
+      return price
+    except ValueError:
             print("Invalid input. Please enter a valid number for Unit Price.")
-            return None
         
-p = True
 
-while p:
+
+def add():
+  p = True
+  while p:
     product_id = input("\nEnter Product ID: ")
-    if product_id in registered_products:
-        print(f"Error: El ID '{product_id}' Already exist.")
-        
-
-    product_name = input("Enter Product Name: ")
+    if product_id != "":
+        if product_id is not None:
+            product_id = int(product_id)
+            if product_id in registered_products:
+                print(f"Error: El ID '{product_id}' Already exist.")
+                continue
+    else:
+       print("Error, enter a ")
+       continue
+       
+    product_name = input("Enter Product Name: ").lower()
     prod_price = unit_price()
 
-    if prod_price is not None:
-        product = (product_id, product_name, prod_price)
+    if product_name and product_id and prod_price is not None:
+        product = (product_name, prod_price)
         registered_products[product_id] = product
         print("Product registered successfully!")
-    else:
-        print("Could not register product due to price errors.")
-    again = input("Do you want to add another product? (y/n): ").lower()
-    if again != 'y':
         p = False
+    else:
+        print("Not into the value request.")
 
-print("\nFinal Inventory:", registered_products)
+34
+add()
