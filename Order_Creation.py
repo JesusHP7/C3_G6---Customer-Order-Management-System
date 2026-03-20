512c14eb5f82f740b844dc7a5f7e07a0c540a2f2
joan <estremorjoan@gmail.com>
2026-03-20 08:42:36 -0500

    confirmations

--- Order_Creation.py
+++ Order_Creation.py
@@ -5,3 +5,12 @@ def agg_order(clients, products):
   if client_id not in clients:
     return "Client not found"
 
+# ask product id
+  product_id = input("Enter product ID: ")
+  while not product_id.isdigit():
+    product_id = input("Enter product ID: ")
+
+  product_id = int(product_id)
+
+  if product_id not in products:
+    return "Product not found"
